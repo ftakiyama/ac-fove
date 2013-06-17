2e42bb2377a80f519fb61cd7eb427a0e34233e4d
Felipe Takiyama <felipe.takiyama@usp.br>
2013-06-17 16:39:42 -0300

    AC-FOVE: documentation
    * ACFOVE: documentation

--- src/br/usp/poli/takiyama/acfove/ACFOVE.java
+++ src/br/usp/poli/takiyama/acfove/ACFOVE.java
@@ -1,6 +1,5 @@
 package br.usp.poli.takiyama.acfove;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -20,16 +19,21 @@ public final class ACFOVE {
 	
 	private final static Logger logger = Logger.getLogger(ACFOVE.class.getName());
 	
-	/**
-	 * This 'buffer' stores all operations that were done in String format.
-	 * Used to avoid loops between inverse operations (expanding/counting)
+	/*
+	 * I need to create a mechanism to avoid deadlocks between expanding and
+	 * counting operations.
+	 * This situation happens when expanding/counting the same PRV alternates
+	 * as the best operation available.
 	 */
-	private final Set<String> pastOperations;
 	
+	/**
+	 * Constructor. Initializes AC-FOVE by shattering the specified marginal on
+	 * the query, shattering all parfactors and converting all aggregation
+	 * parfactors to standard parfactors.
+	 */
 	public ACFOVE(Marginal parfactors) {
 		
 		ConsoleLogger.setup();
-		pastOperations = new HashSet<String>(100);
 		
 		logger.info("Starting AC-FOVE...");
 		this.input = parfactors;
@@ -43,6 +47,15 @@ public final class ACFOVE {
 	}
 	
 
+	/**
+	 * Shatters the specified marginal on the query (preservable of the
+	 * marginal). After that shatters all parfactors in the marginal to 
+	 * guarantee that for each pair of PRVs in different parfactors, sets of
+	 * random variables represented by them are either equal or disjoint.
+	 * 
+	 * @param arg The marginal to shatter.
+	 * @return The specified marginal shattered
+	 */
 	private Marginal performInitialShattering(Marginal arg) {
 		Marginal result = new ShatterOnQuery(arg).run();
 		result = new Shatter(result).run();
@@ -50,15 +63,26 @@ public final class ACFOVE {
 	}
 	
 	
+	/**
+	 * Converts all aggregation parfactors in the specified marginal to 
+	 * standard parfactors.
+	 * @param arg The marginal containing parfactors to convert.
+	 * @return The specified marginal with all aggregation parfactors 
+	 * converted to standard parfactors.
+	 */
 	private Marginal performConversionToStdParfactors(Marginal arg) {
 		return new ConvertToStdParfactors(arg).run();
 	}
 	
 	
+	/**
+	 * Runs the AC-FOVE algorithm and returns the result.
+	 * @return The result of running the AC-FOVE algorithm on the marginal
+	 * specified when creating this instance.
+	 */
 	public Parfactor run() {
 		while (thereAreVariablesToEliminate()) {
 			runStep();
-			storeOperation();
 			resetCurrentOperation();
 		}
 		if (result.size() > 1) {
@@ -69,16 +93,21 @@ public final class ACFOVE {
 	}
 	
 	
+	/**
+	 * Returns <code>true</code> if there are variables to eliminate in the
+	 * marginal. All random variables that are not in the query must be 
+	 * eliminated.
+	 */
 	private boolean thereAreVariablesToEliminate() {
 		return !result.eliminables().isEmpty();
 	}
-
-	
-	private void storeOperation() {
-		pastOperations.add(currentOperation.toString());
-	}
 	
 	
+	/**
+	 * Returns the result of running one step of the algorithm. A step
+	 * consists in choosing a macro operation and executing it.
+	 * @return the result of running one step of the algorithm.
+	 */
 	Marginal runStep() {
 		chooseMacroOperation();
 		executeMacroOperation();
@@ -86,6 +115,10 @@ public final class ACFOVE {
 	}
 	
 	
+	/**
+	 * Chooses the macro operation to execute. The chosen operation must have
+	 * a smaller cost than the current operation. 
+	 */
 	private void chooseMacroOperation() {
 		for (Parfactor p : result) {
 			for (Prv prv : p.prvs()) {
@@ -99,28 +132,10 @@ public final class ACFOVE {
 		}
 	}
 	
-	// TODO improve cost comparison code
-	
 	private void evaluateGlobalSumOut(Prv prv, Set<Constraint> c) {
 		RandomVariableSet eliminables = RandomVariableSet.getInstance(prv, c);
 		MacroOperation candidate = new GlobalSumOut(result, eliminables);
-		
-		logger.info("\nEvaluating candidate " + candidate + " with " + currentOperation + "\n");
-		int candidateCost = candidate.cost();
-		int currentCost = currentOperation.cost();
-		boolean costIsSmaller = (candidateCost < currentCost);
-		boolean costIsEqual = (candidateCost == currentCost);
-		boolean eliminatesMore = (candidate.numberOfRandomVariablesEliminated() 
-				> currentOperation.numberOfRandomVariablesEliminated());
-		
-		logger.info("\n Candidate costs " + candidateCost + " while current costs " + currentCost + "\n");
-		
-		if (costIsSmaller || (costIsEqual && eliminatesMore)) {
-			logger.info("\nSetting " + candidate + " as current operation.\n");
-			currentOperation = candidate;
-		} else {
-			logger.info("\nKeeping " + currentOperation + " as current operation.\n");
-		}
+		compareAndUpdate(candidate);
 	}
 	
 	private void evaluateFullExpand(Parfactor p, Prv prv) {
@@ -141,11 +156,21 @@ public final class ACFOVE {
 	/**
 	 * Compares the candidate macro operation with current operation and
 	 * updates current if candidate's cost is smaller.
+	 * In case of draw, the operation that eliminates more random variables is
+	 * chosen.
+	 * In case of another draw, current operation is kept.
 	 */
 	private void compareAndUpdate(MacroOperation candidate) {
 		logger.info("\nEvaluating candidate " + candidate + " with " + currentOperation + "\n");
-		boolean costIsSmaller = (candidate.cost() < currentOperation.cost());
-		if (costIsSmaller && !pastOperations.contains(candidate.toString())) {
+		int candidateCost = candidate.cost();
+		int currentCost = currentOperation.cost();
+		
+		boolean costIsSmaller = (candidateCost < currentCost);
+		boolean costIsEqual = (candidateCost == currentCost);
+		boolean eliminatesMore = (candidate.numberOfRandomVariablesEliminated() 
+				> currentOperation.numberOfRandomVariablesEliminated());
+		
+		if (costIsSmaller || (costIsEqual && eliminatesMore)) {
 			logger.info("\nSetting " + candidate + " as current operation.\n");
 			currentOperation = candidate;
 		} else {
@@ -153,11 +178,19 @@ public final class ACFOVE {
 		}
 	}
 	
+	
+	/**
+	 * Executes the current macro operation.
+	 * TODO: throw exception when something happens
+	 */
 	private void executeMacroOperation() {
 		logger.info("\nRunning " + currentOperation + "\n");
 		result = currentOperation.run();
 	}
 	
+	/**
+	 * Resets the current macro operation for the next step of the algorithm.
+	 */
 	private void resetCurrentOperation() {
 		currentOperation = ImpossibleOperation.instance;
 	}
